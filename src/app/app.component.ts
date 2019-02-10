e4b7b1002db111032bcafdb0b3a14e25a8444097
tancha <taktanchanok@gmail.com>
2019-02-10 14:36:09 +0700

    'Message'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Card} from './card';
+import { last } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  cards:Card[] = [];
+  
+  get sorted(): Card[]{
+    console.log('access');
+    return this.cards.sort((a, b)=>b.votes - a.votes);
+  }
+
+  onclick(firstname:HTMLInputElement,lastname:HTMLInputElement){
+   
+    const card = new Card(firstname.value,lastname.value,0);
+    this.cards.push(card);
+    console.log(this.cards);
+    firstname.value = '';
+    lastname.value = '';
+    
+  }
 }
---
+++ src/app/card.ts
@@ -0,0 +1,17 @@
+export class Card {
+    firstName: string;
+    lastName: string;
+    votes: number;
+    constructor(
+        firstName: string,
+        lastName: string,
+        votes: number
+        ){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.votes = votes;
+    }
+    get name(): string{
+        return this.firstName +' '+ this.lastName
+    }
+}
